6b3fb70906df7e52b4f516547aed7b05c4e566cb
readleyj <readleyj@gmail.com>
2020-08-21 00:34:22 +0400

    Implemented Color, color operations and added unit tests

---
+++ src/color.go
@@ -0,0 +1,45 @@
+package internal
+
+type Color struct {
+	R, G, B float64
+}
+
+func AddColors(c1, c2 Color) Color {
+	return Color{c1.R + c2.R, c1.G + c2.G, c1.B + c2.B}
+}
+
+func (c1 Color) Add(c2 Color) Color {
+	return Color{c1.R + c2.R, c1.G + c2.G, c1.B + c2.B}
+}
+
+func SubColors(c1, c2 Color) Color {
+	return Color{c1.R - c2.R, c1.G - c2.G, c1.B - c2.B}
+}
+
+func (c1 Color) Sub(c2 Color) Color {
+	return Color{c1.R - c2.R, c1.G - c2.G, c1.B - c2.B}
+}
+
+func ColorScalarMultiply(c1 Color, scalar float64) Color {
+	return Color{c1.R * scalar, c1.G * scalar, c1.B * scalar}
+}
+
+func (c1 Color) MultiplyByScalar(scalar float64) Color {
+	return Color{c1.R * scalar, c1.G * scalar, c1.B * scalar}
+}
+
+func HadamardProduct(c1, c2 Color) Color {
+	return Color{
+		c1.R * c2.R,
+		c1.G * c2.G,
+		c1.B * c2.B,
+	}
+}
+
+func (c1 Color) HadamardProduct(c2 Color) Color {
+	return Color{
+		c1.R * c2.R,
+		c1.G * c2.G,
+		c1.B * c2.B,
+	}
+}
---
+++ src/color_test.go
@@ -0,0 +1,54 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewColor(t *testing.T) {
+	c := Color{-0.5, 0.4, 1.7}
+
+	assert.InDelta(t, -0.5, c.R, float64EqualityThreshold)
+	assert.InDelta(t, 0.4, c.G, float64EqualityThreshold)
+	assert.InDelta(t, 1.7, c.B, float64EqualityThreshold)
+}
+
+func TestColorAdd(t *testing.T) {
+	c1 := Color{0.9, 0.6, 0.75}
+	c2 := Color{0.7, 0.1, 0.25}
+	c3 := AddColors(c1, c2)
+
+	assert.InDelta(t, 1.6, c3.R, float64EqualityThreshold)
+	assert.InDelta(t, 0.7, c3.G, float64EqualityThreshold)
+	assert.InDelta(t, 1.0, c3.B, float64EqualityThreshold)
+}
+
+func TestColorSub(t *testing.T) {
+	c1 := Color{0.9, 0.6, 0.75}
+	c2 := Color{0.7, 0.1, 0.25}
+	c3 := SubColors(c1, c2)
+
+	assert.InDelta(t, 0.2, c3.R, float64EqualityThreshold)
+	assert.InDelta(t, 0.5, c3.G, float64EqualityThreshold)
+	assert.InDelta(t, 0.5, c3.B, float64EqualityThreshold)
+}
+
+func TestMultiplyColorByScalar(t *testing.T) {
+	c1 := Color{0.2, 0.3, 0.4}
+	c2 := ColorScalarMultiply(c1, 2.0)
+
+	assert.InDelta(t, 0.4, c2.R, float64EqualityThreshold)
+	assert.InDelta(t, 0.6, c2.G, float64EqualityThreshold)
+	assert.InDelta(t, 0.8, c2.B, float64EqualityThreshold)
+}
+
+func TestHadamardProduct(t *testing.T) {
+	c1 := Color{1.0, 0.2, 0.4}
+	c2 := Color{0.9, 1.0, 0.1}
+	c3 := HadamardProduct(c1, c2)
+
+	assert.InDelta(t, 0.9, c3.R, float64EqualityThreshold)
+	assert.InDelta(t, 0.2, c3.G, float64EqualityThreshold)
+	assert.InDelta(t, 0.04, c3.B, float64EqualityThreshold)
+}
