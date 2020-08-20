f822bb50b9c50e0705a5d7b5395da0641fe10e06
readleyj <readleyj@gmail.com>
2020-08-20 23:54:30 +0400

    Renamed receiver names for consistency

--- src/tuple.go
+++ src/tuple.go
@@ -18,12 +18,12 @@ func Point(x, y, z float64) Tuple {
 	return Tuple{x, y, z, 1.0}
 }
 
-func (t Tuple) IsVector() bool {
-	return t.W == 0.0
+func (t1 Tuple) IsVector() bool {
+	return t1.W == 0.0
 }
 
-func (t Tuple) IsPoint() bool {
-	return t.W == 1.0
+func (t1 Tuple) IsPoint() bool {
+	return t1.W == 1.0
 }
 
 func TupleEquals(t1, t2 Tuple) bool {
@@ -54,32 +54,32 @@ func Negate(t1 Tuple) Tuple {
 	return Tuple{-t1.X, -t1.Y, -t1.Z, -t1.W}
 }
 
-func (t Tuple) Negate() Tuple {
-	return Tuple{-t.X, -t.Y, -t.Z, -t.W}
+func (t1 Tuple) Negate() Tuple {
+	return Tuple{-t1.X, -t1.Y, -t1.Z, -t1.W}
 }
 
 func MultiplyByScalar(t1 Tuple, scalar float64) Tuple {
 	return Tuple{t1.X * scalar, t1.Y * scalar, t1.Z * scalar, t1.W * scalar}
 }
 
-func (t Tuple) MultiplyByScalar(scalar float64) Tuple {
-	return Tuple{t.X * scalar, t.Y * scalar, t.Z * scalar, t.W * scalar}
+func (t1 Tuple) MultiplyByScalar(scalar float64) Tuple {
+	return Tuple{t1.X * scalar, t1.Y * scalar, t1.Z * scalar, t1.W * scalar}
 }
 
 func DivideByScalar(t1 Tuple, scalar float64) Tuple {
 	return Tuple{t1.X / scalar, t1.Y / scalar, t1.Z / scalar, t1.W / scalar}
 }
 
-func (t Tuple) DivideByScalar(scalar float64) Tuple {
-	return Tuple{t.X / scalar, t.Y / scalar, t.Z / scalar, t.W / scalar}
+func (t1 Tuple) DivideByScalar(scalar float64) Tuple {
+	return Tuple{t1.X / scalar, t1.Y / scalar, t1.Z / scalar, t1.W / scalar}
 }
 
 func Magnitude(t1 Tuple) float64 {
 	return math.Sqrt(t1.X*t1.X + t1.Y*t1.Y + t1.Z*t1.Z)
 }
 
-func (t Tuple) Magnitude() float64 {
-	return math.Sqrt(t.X*t.X + t.Y*t.Y + t.Z*t.Z)
+func (t1 Tuple) Magnitude() float64 {
+	return math.Sqrt(t1.X*t1.X + t1.Y*t1.Y + t1.Z*t1.Z)
 }
 
 func Normalize(t1 Tuple) Tuple {
@@ -87,9 +87,9 @@ func Normalize(t1 Tuple) Tuple {
 	return Tuple{t1.X / magnitude, t1.Y / magnitude, t1.Z / magnitude, t1.W / magnitude}
 }
 
-func (t Tuple) Normalize() Tuple {
-	magnitude := Magnitude(t)
-	return Tuple{t.X / magnitude, t.Y / magnitude, t.Z / magnitude, t.W / magnitude}
+func (t1 Tuple) Normalize() Tuple {
+	magnitude := Magnitude(t1)
+	return Tuple{t1.X / magnitude, t1.Y / magnitude, t1.Z / magnitude, t1.W / magnitude}
 }
 
 func Dot(t1, t2 Tuple) float64 {
