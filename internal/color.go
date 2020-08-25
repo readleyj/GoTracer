70e2c9efb2cfdaad0f13f582341fdb6a69fb04ba
readleyj <readleyj@gmail.com>
2020-08-25 16:56:54 +0400

    Added ColorEquals function and Equals method to Color

--- internal/color.go
+++ internal/color.go
@@ -1,5 +1,7 @@
 package internal
 
+import "github.com/google/go-cmp/cmp"
+
 type Color struct {
 	R, G, B float64
 }
@@ -30,6 +32,14 @@ func ColorScalarMultiply(c1 Color, scalar float64) Color {
 
 func (c1 Color) MultiplyByScalar(scalar float64) Color {
 	return NewColor(c1.R*scalar, c1.G*scalar, c1.B*scalar)
+}
+
+func ColorEquals(t1, t2 Color) bool {
+	return cmp.Equal(t1, t2, opt)
+}
+
+func (t1 Color) Equals(t2 Color) bool {
+	return cmp.Equal(t1, t2, opt)
 }
 
 func HadamardProduct(c1, c2 Color) Color {
