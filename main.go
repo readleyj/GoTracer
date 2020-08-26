0717bdb239b1209369b37b4752854e3fc8203678
readleyj <readleyj@gmail.com>
2020-08-26 22:25:31 +0400

    Added ppm file from the end of Chapter 6

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"gotracer/internal"
 	"io/ioutil"
+	"math"
 )
 
 func RenderCircle() {
@@ -80,7 +81,93 @@ func RenderSphere() {
 	ioutil.WriteFile("sphere.ppm", []byte(canvas.ToPPM()), 0)
 }
 
+func RenderScene() {
+	world := internal.NewWorld()
+
+	floor := internal.NewSphere()
+	floor.SetTransform(internal.Scale(10, 0.1, 10))
+	floor.SetMaterial(internal.NewDefaultMaterial())
+	floor.Material.SetColor(internal.NewColor(1, 0.9, 0.9))
+	floor.Material.Specular = 0
+	world.Objects = append(world.Objects, floor)
+
+	leftWall := internal.NewSphere()
+	leftTransform := internal.MatrixMultiply(
+		internal.MatrixMultiply(
+			internal.MatrixMultiply(
+				internal.Translate(0, 0, 5),
+				internal.RotateY(-math.Pi/4),
+			),
+			internal.RotateX(math.Pi/2),
+		),
+		internal.Scale(10, 0.1, 10),
+	)
+	leftWall.SetTransform(leftTransform)
+	world.Objects = append(world.Objects, leftWall)
+
+	rightWall := internal.NewSphere()
+	rightTransform := internal.MatrixMultiply(
+		internal.MatrixMultiply(
+			internal.MatrixMultiply(
+				internal.Translate(0, 0, 5),
+				internal.RotateY(math.Pi/4),
+			),
+			internal.RotateX(math.Pi/2),
+		),
+		internal.Scale(10, 0.1, 10),
+	)
+	rightWall.SetTransform(rightTransform)
+	world.Objects = append(world.Objects, rightWall)
+
+	middle := internal.NewSphere()
+	middle.SetTransform(internal.Translate(-0.5, 1, 0.5))
+	middle.SetMaterial(internal.NewDefaultMaterial())
+	middle.Material.SetColor(internal.NewColor(0.1, 1, 0.5))
+	middle.Material.Diffuse = 0.7
+	middle.Material.Specular = 0.3
+	world.Objects = append(world.Objects, middle)
+
+	right := internal.NewSphere()
+	right.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(1.5, 0.5, -0.5),
+			internal.Scale(0.5, 0.5, 0.5),
+		),
+	)
+	right.SetMaterial(internal.NewDefaultMaterial())
+	right.Material.SetColor(internal.NewColor(0.5, 1, 0.1))
+	right.Material.Diffuse = 0.7
+	right.Material.Specular = 0.3
+	world.Objects = append(world.Objects, right)
+
+	left := internal.NewSphere()
+	left.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(-1.5, 0.33, -0.75),
+			internal.Scale(0.33, 0.33, 0.33),
+		),
+	)
+	left.SetMaterial(internal.NewDefaultMaterial())
+	left.Material.SetColor(internal.NewColor(1.0, 0.8, 0.1))
+	left.Material.Diffuse = 0.7
+	left.Material.Specular = 0.3
+	world.Objects = append(world.Objects, left)
+
+	world.Lights = append(world.Lights, internal.NewPointLight(internal.NewPoint(-10, 10, -10), internal.NewColor(1, 1, 1)))
+
+	camera := internal.NewCamera(100, 50, math.Pi/3)
+	camera.Transform = internal.ViewTransform(
+		internal.NewPoint(0, 1.5, -5),
+		internal.NewPoint(0, 1, 0),
+		internal.NewVector(0, 1, 0),
+	)
+
+	canvas := internal.Render(camera, world)
+	ioutil.WriteFile("scene.ppm", []byte(canvas.ToPPM()), 0)
+}
+
 func main() {
-	RenderCircle()
+	// RenderCircle()
 	// RenderSphere()
+	RenderScene()
 }
