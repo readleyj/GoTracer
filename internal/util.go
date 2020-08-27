8d5c1326d8ac0faeae49160a4c97d63de027e206
readleyj <readleyj@gmail.com>
2020-08-28 00:48:59 +0400

    Updated equality threshold

--- internal/util.go
+++ internal/util.go
@@ -4,6 +4,6 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-const float64EqualityThreshold = 1e-5
+const float64EqualityThreshold = 1e-4
 
 var opt = cmpopts.EquateApprox(0, float64EqualityThreshold)
