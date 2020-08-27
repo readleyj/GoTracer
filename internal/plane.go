d6f8dfa796755e6e7cf8fee62c856d63cdd64ae1
readleyj <readleyj@gmail.com>
2020-08-27 17:01:09 +0400

    Implemented Plane and corresponding unit tests

---
+++ internal/plane.go
@@ -0,0 +1,58 @@
+package internal
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+type Plane struct {
+	ID        int64
+	Material  Material
+	Transform Matrix
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func NewPlane() *Plane {
+	return &Plane{
+		rand.Int63(),
+		NewDefaultMaterial(),
+		NewIdentity4(),
+	}
+}
+
+func (p *Plane) GetID() int64 {
+	return p.ID
+}
+
+func (p *Plane) LocalIntersect(localRay Ray) Intersections {
+	if math.Abs(localRay.Direction.Y) < float64EqualityThreshold {
+		return Intersections{}
+	}
+
+	t := -localRay.Origin.Y / localRay.Direction.Y
+	return NewIntersections(NewIntersection(t, p))
+}
+
+func (p *Plane) LocalNormalAt(point Tuple) Tuple {
+	return NewVector(0, 1, 0)
+}
+
+func (p *Plane) GetTransform() Matrix {
+	return p.Transform
+}
+
+func (p *Plane) SetTransform(transform Matrix) {
+	p.Transform = transform
+}
+
+func (p *Plane) GetMaterial() Material {
+	return p.Material
+}
+
+func (p *Plane) SetMaterial(material Material) {
+	p.Material = material
+}
---
+++ internal/plane_test.go
@@ -0,0 +1,54 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPlaneNormalIsConstantEverywhere(t *testing.T) {
+	p := NewPlane()
+	n1 := p.LocalNormalAt(NewPoint(0, 0, 0))
+	n2 := p.LocalNormalAt(NewPoint(10, 0, -10))
+	n3 := p.LocalNormalAt(NewPoint(-5, 0, 150))
+
+	assert.True(t, TupleEquals(NewVector(0, 1, 0), n1))
+	assert.True(t, TupleEquals(NewVector(0, 1, 0), n2))
+	assert.True(t, TupleEquals(NewVector(0, 1, 0), n3))
+}
+
+func TestPlaneIntersectRayParallel(t *testing.T) {
+	p := NewPlane()
+	r := NewRay(NewPoint(0, 10, 0), NewVector(0, 0, 1))
+	xs := p.LocalIntersect(r)
+
+	assert.Equal(t, 0, len(xs))
+}
+
+func TestPlaneIntersectRayCoplanar(t *testing.T) {
+	p := NewPlane()
+	r := NewRay(NewPoint(0, 0, 0), NewVector(0, 0, 1))
+	xs := p.LocalIntersect(r)
+
+	assert.Equal(t, 0, len(xs))
+}
+
+func TestRayIntersectsPlaneFromAbove(t *testing.T) {
+	p := NewPlane()
+	r := NewRay(NewPoint(0, 1, 0), NewVector(0, -1, 0))
+	xs := p.LocalIntersect(r)
+
+	assert.Equal(t, 1, len(xs))
+	assert.InDelta(t, 1.0, xs[0].T, float64EqualityThreshold)
+	assert.True(t, ShapeEquals(p, xs[0].Object))
+}
+
+func TestRayIntersectsPlaneFromBelow(t *testing.T) {
+	p := NewPlane()
+	r := NewRay(NewPoint(0, -1, 0), NewVector(0, 1, 0))
+	xs := p.LocalIntersect(r)
+
+	assert.Equal(t, 1, len(xs))
+	assert.InDelta(t, 1.0, xs[0].T, float64EqualityThreshold)
+	assert.True(t, ShapeEquals(p, xs[0].Object))
+}
