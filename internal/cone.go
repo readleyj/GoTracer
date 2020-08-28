7f49106d3c03f929bbe853494c70f87c6d7c19a4
readleyj <readleyj@gmail.com>
2020-08-28 20:49:23 +0400

    Implemented Cone and corresponding unit tests

---
+++ internal/cone.go
@@ -0,0 +1,136 @@
+package internal
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+type Cone struct {
+	ID        int64
+	Material  Material
+	Transform Matrix
+	Minimum   float64
+	Maximum   float64
+	Closed    bool
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func NewCone() *Cone {
+	return &Cone{
+		rand.Int63(),
+		NewDefaultMaterial(),
+		NewIdentity4(),
+		math.Inf(-1),
+		math.Inf(1),
+		false,
+	}
+}
+
+func (cone *Cone) GetID() int64 {
+	return cone.ID
+}
+
+func (cone *Cone) LocalIntersect(ray Ray) Intersections {
+	var xs []Intersection
+
+	direction, origin := ray.Direction, ray.Origin
+	a := math.Pow(direction.X, 2) - math.Pow(direction.Y, 2) + math.Pow(direction.Z, 2)
+	b := 2*origin.X*direction.X - 2*origin.Y*direction.Y + 2*origin.Z*direction.Z
+	c := math.Pow(origin.X, 2) - math.Pow(origin.Y, 2) + math.Pow(origin.Z, 2)
+	discriminant := b*b - 4*a*c
+
+	if (math.Abs(a) < float64EqualityThreshold && math.Abs(b) < float64EqualityThreshold) || discriminant < 0 {
+		return xs
+	}
+
+	var t1, t2 float64
+
+	if math.Abs(a) < float64EqualityThreshold {
+		t1 = -c / (2 * b)
+		y1 := ray.Origin.Y + t1*ray.Direction.Y
+
+		if y1 > cone.Minimum && y1 < cone.Maximum {
+			xs = append(xs, NewIntersection(t1, cone))
+		}
+
+	} else {
+		t1, t2 = (-b-math.Sqrt(discriminant))/(2*a), (-b+math.Sqrt(discriminant))/(2*a)
+
+		y1 := ray.Origin.Y + t1*ray.Direction.Y
+		if cone.Minimum < y1 && y1 < cone.Maximum {
+			xs = append(xs, NewIntersection(t1, cone))
+		}
+
+		y2 := ray.Origin.Y + t2*ray.Direction.Y
+		if cone.Minimum < y2 && y2 < cone.Maximum {
+			xs = append(xs, NewIntersection(t2, cone))
+		}
+	}
+
+	return cone.IntersectCaps(ray, xs)
+}
+
+func (cone *Cone) LocalNormalAt(point Tuple) Tuple {
+	dist := point.X*point.X + point.Z*point.Z
+
+	if dist < 1 && point.Y >= cone.Maximum-float64EqualityThreshold {
+		return NewVector(0, 1, 0)
+	} else if dist < 1 && point.Y <= cone.Minimum+float64EqualityThreshold {
+		return NewVector(0, -1, 0)
+	}
+
+	y := math.Sqrt(dist)
+
+	if point.Y > 0 {
+		y = -y
+	}
+
+	return NewVector(point.X, y, point.Z)
+}
+
+func (cone *Cone) GetTransform() Matrix {
+	return cone.Transform
+}
+
+func (cone *Cone) SetTransform(transform Matrix) {
+	cone.Transform = transform
+}
+
+func (cone *Cone) GetMaterial() Material {
+	return cone.Material
+}
+
+func (cone *Cone) SetMaterial(material Material) {
+	cone.Material = material
+}
+
+func (cone *Cone) IntersectCaps(ray Ray, xs Intersections) Intersections {
+	var t float64
+
+	if !cone.Closed || math.Abs(ray.Direction.Y) < float64EqualityThreshold {
+		return xs
+	}
+
+	t = (cone.Minimum - ray.Origin.Y) / ray.Direction.Y
+	if cone.CheckCap(ray, t, cone.Minimum) {
+		xs = append(xs, NewIntersection(t, cone))
+	}
+
+	t = (cone.Maximum - ray.Origin.Y) / ray.Direction.Y
+	if cone.CheckCap(ray, t, cone.Maximum) {
+		xs = append(xs, NewIntersection(t, cone))
+	}
+
+	return xs
+}
+
+func (cone *Cone) CheckCap(r Ray, t float64, bound float64) bool {
+	x := r.Origin.X + t*r.Direction.X
+	z := r.Origin.Z + t*r.Direction.Z
+
+	return (x*x + z*z) <= math.Abs(bound)
+}
---
+++ internal/cone_test.go
@@ -0,0 +1,88 @@
+package internal
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRayIntersectCone(t *testing.T) {
+	testCases := []struct {
+		origin    Tuple
+		direction Tuple
+		t0        float64
+		t1        float64
+	}{
+		{NewPoint(0, 0, -5), NewVector(0, 0, 1), 5, 5},
+		{NewPoint(0, 0, -5), NewVector(1, 1, 1), 8.66025, 8.66025},
+		{NewPoint(1, 1, -5), NewVector(-0.5, -1, 1), 4.55006, 49.44994},
+	}
+
+	cone := NewCone()
+
+	for _, test := range testCases {
+		direction := Normalize(test.direction)
+		ray := NewRay(test.origin, direction)
+		xs := cone.LocalIntersect(ray)
+
+		assert.Equal(t, 2, len(xs))
+		assert.InDelta(t, test.t0, xs[0].T, float64EqualityThreshold)
+		assert.InDelta(t, test.t1, xs[1].T, float64EqualityThreshold)
+	}
+}
+
+func TestIntersectConeWithParallelRay(t *testing.T) {
+	cone := NewCone()
+
+	direction := Normalize(NewVector(0, 1, 1))
+	ray := NewRay(NewPoint(0, 0, -1), direction)
+	xs := cone.LocalIntersect(ray)
+
+	assert.Equal(t, 1, len(xs))
+	assert.InDelta(t, 0.35355, xs[0].T, float64EqualityThreshold)
+}
+
+func TestConeIntersectEndCaps(t *testing.T) {
+	testCases := []struct {
+		origin    Tuple
+		direction Tuple
+		count     int
+	}{
+		{NewPoint(0, 0, -5), NewVector(0, 1, 0), 0},
+		{NewPoint(0, 0, -0.25), NewVector(0, 1, 1), 2},
+		{NewPoint(0, 0, -0.25), NewVector(0, 1, 0), 4},
+	}
+
+	cone := NewCone()
+	cone.Minimum = -0.5
+	cone.Maximum = 0.5
+	cone.Closed = true
+
+	for _, test := range testCases {
+		direction := Normalize(test.direction)
+		ray := NewRay(test.origin, direction)
+		xs := cone.LocalIntersect(ray)
+
+		assert.Equal(t, test.count, len(xs))
+	}
+}
+
+func TestNormalOnCone(t *testing.T) {
+	testCases := []struct {
+		point  Tuple
+		normal Tuple
+	}{
+		{NewPoint(0, 0, 0), NewVector(0, 0, 0)},
+		{NewPoint(1, 1, 1), NewVector(1, -math.Sqrt(2), 1)},
+		{NewPoint(-1, -1, 0), NewVector(-1, 1, 0)},
+	}
+
+	cone := NewCone()
+
+	for _, test := range testCases {
+		n := cone.LocalNormalAt(test.point)
+
+		assert.True(t, TupleEquals(test.normal, n))
+	}
+}
