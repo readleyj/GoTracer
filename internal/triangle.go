a17a6858871c97f8556c9233134c45ba79197263
readleyj <readleyj@gmail.com>
2020-08-29 20:41:37 +0400

    Implemented Triangle primitive and unit tests

---
+++ internal/triangle.go
@@ -0,0 +1,98 @@
+package internal
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+type Triangle struct {
+	ID         int64
+	Material   Material
+	Transform  Matrix
+	Parent     *Group
+	P1, P2, P3 Tuple
+	E1, E2     Tuple
+	Normal     Tuple
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func NewTriangle(p1, p2, p3 Tuple) *Triangle {
+	e1, e2 := SubTuples(p2, p1), SubTuples(p3, p1)
+
+	return &Triangle{
+		rand.Int63(),
+		NewDefaultMaterial(),
+		NewIdentity4(),
+		nil,
+		p1,
+		p2,
+		p3,
+		e1,
+		e2,
+		Normalize(Cross(e2, e1)),
+	}
+}
+
+func (tri *Triangle) GetID() int64 {
+	return tri.ID
+}
+
+func (tri *Triangle) LocalIntersect(localRay Ray) Intersections {
+	dirCrossE2 := Cross(localRay.Direction, tri.E2)
+	det := Dot(tri.E1, dirCrossE2)
+
+	if math.Abs(det) < float64EqualityThreshold {
+		return Intersections{}
+	}
+
+	f := 1.0 / det
+
+	p1ToOrigin := SubTuples(localRay.Origin, tri.P1)
+	u := f * Dot(p1ToOrigin, dirCrossE2)
+
+	if u < 0 || u > 1 {
+		return Intersections{}
+	}
+
+	originCrossE1 := Cross(p1ToOrigin, tri.E1)
+	v := f * Dot(localRay.Direction, originCrossE1)
+
+	if v < 0 || (u+v) > 1 {
+		return Intersections{}
+	}
+
+	t := f * Dot(tri.E2, originCrossE1)
+	return NewIntersections(NewIntersection(t, tri))
+}
+
+func (tri *Triangle) LocalNormalAt(point Tuple) Tuple {
+	return tri.Normal
+}
+
+func (tri *Triangle) GetTransform() Matrix {
+	return tri.Transform
+}
+
+func (tri *Triangle) SetTransform(transform Matrix) {
+	tri.Transform = transform
+}
+
+func (tri *Triangle) GetMaterial() Material {
+	return tri.Material
+}
+
+func (tri *Triangle) SetMaterial(material Material) {
+	tri.Material = material
+}
+
+func (tri *Triangle) GetParent() *Group {
+	return tri.Parent
+}
+
+func (tri *Triangle) SetParent(g *Group) {
+	tri.Parent = g
+}
---
+++ internal/triangle_test.go
@@ -0,0 +1,74 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateTriangle(t *testing.T) {
+	p1 := NewPoint(0, 1, 0)
+	p2 := NewPoint(-1, 0, 0)
+	p3 := NewPoint(1, 0, 0)
+	tri := NewTriangle(p1, p2, p3)
+
+	assert.True(t, TupleEquals(p1, tri.P1))
+	assert.True(t, TupleEquals(p2, tri.P2))
+	assert.True(t, TupleEquals(p3, tri.P3))
+	assert.True(t, TupleEquals(NewVector(-1, -1, 0), tri.E1))
+	assert.True(t, TupleEquals(NewVector(1, -1, 0), tri.E2))
+	assert.True(t, TupleEquals(NewVector(0, 0, -1), tri.Normal))
+}
+
+func TestFindNormalOnTriangle(t *testing.T) {
+	tri := NewTriangle(NewPoint(0, 1, 0), NewPoint(-1, 0, 0), NewPoint(1, 0, 0))
+
+	n1 := tri.LocalNormalAt(NewPoint(0, 0.5, 0))
+	n2 := tri.LocalNormalAt(NewPoint(-0.5, 0.75, 0))
+	n3 := tri.LocalNormalAt(NewPoint(0.5, 0.25, 0))
+
+	assert.True(t, TupleEquals(n1, tri.Normal))
+	assert.True(t, TupleEquals(n2, tri.Normal))
+	assert.True(t, TupleEquals(n3, tri.Normal))
+}
+
+func TestIntersectRayParallelToTriangle(t *testing.T) {
+	tri := NewTriangle(NewPoint(0, 1, 0), NewPoint(-1, 0, 0), NewPoint(1, 0, 0))
+	r := NewRay(NewPoint(0, -1, -2), NewVector(0, 1, 0))
+	xs := tri.LocalIntersect(r)
+
+	assert.Equal(t, 0, len(xs))
+}
+
+func TestRayMissesEdgeP13(t *testing.T) {
+	tri := NewTriangle(NewPoint(0, 1, 0), NewPoint(-1, 0, 0), NewPoint(1, 0, 0))
+	r := NewRay(NewPoint(1, 1, -2), NewVector(0, 0, 1))
+	xs := tri.LocalIntersect(r)
+
+	assert.Equal(t, 0, len(xs))
+}
+
+func TestRayMissesEdgeP12(t *testing.T) {
+	tri := NewTriangle(NewPoint(0, 1, 0), NewPoint(-1, 0, 0), NewPoint(1, 0, 0))
+	r := NewRay(NewPoint(-1, 1, -2), NewVector(0, 0, 1))
+	xs := tri.LocalIntersect(r)
+
+	assert.Equal(t, 0, len(xs))
+}
+
+func TestRayMissesEdgeP23(t *testing.T) {
+	tri := NewTriangle(NewPoint(0, 1, 0), NewPoint(-1, 0, 0), NewPoint(1, 0, 0))
+	r := NewRay(NewPoint(0, -1, -2), NewVector(0, 0, 1))
+	xs := tri.LocalIntersect(r)
+
+	assert.Equal(t, 0, len(xs))
+}
+
+func TestRayStrikesTriangle(t *testing.T) {
+	tri := NewTriangle(NewPoint(0, 1, 0), NewPoint(-1, 0, 0), NewPoint(1, 0, 0))
+	r := NewRay(NewPoint(0, 0.5, -2), NewVector(0, 0, 1))
+	xs := tri.LocalIntersect(r)
+
+	assert.Equal(t, 1, len(xs))
+	assert.InDelta(t, 2.0, xs[0].T, float64EqualityThreshold)
+}
