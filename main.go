fe92e8e422f2ade2ebc7bd6f7b90740de046e694
readleyj <readleyj@gmail.com>
2020-09-17 20:05:41 +0400

    Added render

--- main.go
+++ main.go
@@ -573,12 +573,311 @@ func renderReflectionRefraction() {
 	writeToPng(canvas, "reflection_refraction.png")
 }
 
+func renderTable() {
+	world := internal.NewWorld()
+
+	camera := internal.NewCamera(1920, 1080, 0.785)
+	camera.Transform = internal.ViewTransform(internal.NewPoint(8, 6, -8), internal.NewPoint(0, 3, 0), internal.NewVector(0, 1, 0))
+
+	light := internal.NewPointLight(internal.NewPoint(0, 6.9, -5), internal.NewColor(1, 1, 0.9))
+
+	floor := internal.NewCube()
+	floorMaterial := internal.NewDefaultMaterial()
+	floorPattern := internal.NewCheckersPattern(
+		internal.NewColor(0, 0, 0),
+		internal.NewColor(0.25, 0.25, 0.25),
+	)
+	floorPattern.SetTransform(internal.Scale(0.07, 0.07, 0.07))
+	floorMaterial.Ambient = 0.25
+	floorMaterial.Diffuse = 0.7
+	floorMaterial.Specular = 0.9
+	floorMaterial.Shininess = 300
+	floorMaterial.Reflective = 0.1
+	floorMaterial.SetPattern(floorPattern)
+	floor.SetMaterial(floorMaterial)
+	floor.SetTransform(
+		internal.MatrixMultiply(
+			internal.Scale(20, 7, 20),
+			internal.Translate(0, 1, 0),
+		),
+	)
+
+	wall := internal.NewCube()
+	wallMaterial := internal.NewDefaultMaterial()
+	wallPattern := internal.NewCheckersPattern(
+		internal.NewColor(0.4863, 0.3765, 0.2941),
+		internal.NewColor(0.3725, 0.2902, 0.2275),
+	)
+	wallPattern.SetTransform(internal.Scale(0.05, 20, 0.05))
+	wallMaterial.Ambient = 0.1
+	wallMaterial.Diffuse = 0.7
+	wallMaterial.Specular = 0.9
+	wallMaterial.Shininess = 300
+	wallMaterial.Reflective = 0.1
+	wallMaterial.SetPattern(wallPattern)
+	wall.SetMaterial(wallMaterial)
+	wall.SetTransform(internal.Scale(10, 10, 10))
+
+	tableTop := internal.NewCube()
+	tableMaterial := internal.NewDefaultMaterial()
+	tablePattern := internal.NewStripePattern(
+		internal.NewColor(0.5529, 0.4235, 0.3255),
+		internal.NewColor(0.6588, 0.5098, 0.4000),
+	)
+	tablePattern.SetTransform(
+		internal.MatrixMultiply(
+			internal.Scale(0.05, 0.05, 0.05),
+			internal.RotateY(0.1),
+		),
+	)
+	tableMaterial.Ambient = 0.1
+	tableMaterial.Diffuse = 0.7
+	tableMaterial.Specular = 0.9
+	tableMaterial.Shininess = 300
+	tableMaterial.Reflective = 0.2
+	tableMaterial.SetPattern(tablePattern)
+	tableTop.SetMaterial(tableMaterial)
+	tableTop.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(0, 3.1, 0),
+			internal.Scale(3, 0.1, 2),
+		),
+	)
+
+	legMaterial := internal.NewDefaultMaterial()
+	legMaterial.SetColor(internal.NewColor(0.5529, 0.4235, 0.3255))
+	legMaterial.Ambient = 0.2
+	legMaterial.Diffuse = 0.7
+
+	leg1 := internal.NewCube()
+	leg1.SetMaterial(legMaterial)
+	leg1.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(2.7, 1.5, -1.7),
+			internal.Scale(0.1, 1.5, 0.1),
+		),
+	)
+
+	leg2 := internal.NewCube()
+	leg2.SetMaterial(legMaterial)
+	leg2.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(2.7, 1.5, 1.7),
+			internal.Scale(0.1, 1.5, 0.1),
+		),
+	)
+
+	leg3 := internal.NewCube()
+	leg3.SetMaterial(legMaterial)
+	leg3.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(-2.7, 1.5, -1.7),
+			internal.Scale(0.1, 1.5, 0.1),
+		),
+	)
+
+	leg4 := internal.NewCube()
+	leg4.SetMaterial(legMaterial)
+	leg4.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(-2.7, 1.5, 1.7),
+			internal.Scale(0.1, 1.5, 0.1),
+		),
+	)
+
+	glassCube := internal.NewCube()
+	// Setting to false makes the cube disappear for some reason
+	glassCube.HasShadow = false
+	glassCube.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(0, 3.45001, 0),
+			internal.MatrixMultiply(
+				internal.RotateY(0.2),
+				internal.Scale(0.25, 0.25, 0.25),
+			),
+		),
+	)
+	glassCubeMaterial := internal.NewDefaultMaterial()
+	glassCubeMaterial.SetColor(internal.NewColor(1, 1, 0.8))
+	glassCubeMaterial.Ambient = 0.0
+	glassCubeMaterial.Diffuse = 0.3
+	glassCubeMaterial.Specular = 0.9
+	glassCubeMaterial.Shininess = 300
+	glassCubeMaterial.Reflective = 0.7
+	glassCubeMaterial.Transparency = 0.7
+	glassCubeMaterial.RefractiveIndex = 1.5
+	glassCube.SetMaterial(glassCubeMaterial)
+
+	cube1 := internal.NewCube()
+	cube1.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(1, 3.35, -0.9),
+			internal.MatrixMultiply(
+				internal.RotateY(-0.4),
+				internal.Scale(0.15, 0.15, 0.15),
+			),
+		),
+	)
+	cube1Material := internal.NewDefaultMaterial()
+	cube1Material.SetColor(internal.NewColor(1, 0.5, 0.5))
+	cube1Material.Reflective = 0.6
+	cube1Material.Diffuse = 0.4
+	cube1.SetMaterial(cube1Material)
+
+	cube2 := internal.NewCube()
+	cube2.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(-1.5, 3.27, 0.3),
+			internal.MatrixMultiply(
+				internal.RotateY(0.4),
+				internal.Scale(0.15, 0.07, 0.15),
+			),
+		),
+	)
+	cube2Material := internal.NewDefaultMaterial()
+	cube2Material.SetColor(internal.NewColor(1, 1, 0.5))
+	cube2.SetMaterial(cube2Material)
+
+	cube3 := internal.NewCube()
+	cube3.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(0, 3.25, 1),
+			internal.MatrixMultiply(
+				internal.RotateY(0.4),
+				internal.Scale(0.2, 0.05, 0.05),
+			),
+		),
+	)
+	cube3Material := internal.NewDefaultMaterial()
+	cube3Material.SetColor(internal.NewColor(0.5, 1, 0.5))
+	cube3.SetMaterial(cube3Material)
+
+	cube4 := internal.NewCube()
+	cube4.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(-0.6, 3.4, -1),
+			internal.MatrixMultiply(
+				internal.RotateY(0.8),
+				internal.Scale(0.05, 0.2, 0.05),
+			),
+		),
+	)
+	cube4Material := internal.NewDefaultMaterial()
+	cube4Material.SetColor(internal.NewColor(0.5, 0.5, 1))
+	cube4.SetMaterial(cube4Material)
+
+	cube5 := internal.NewCube()
+	cube5.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(2, 3.4, 1),
+			internal.MatrixMultiply(
+				internal.RotateY(0.8),
+				internal.Scale(0.05, 0.2, 0.05),
+			),
+		),
+	)
+	cube5Material := internal.NewDefaultMaterial()
+	cube5Material.SetColor(internal.NewColor(0.5, 1, 1))
+	cube5.SetMaterial(cube5Material)
+
+	frame1 := internal.NewCube()
+	frame1.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(-10, 4, 1),
+			internal.Scale(0.05, 1, 1),
+		),
+	)
+	frame1Material := internal.NewDefaultMaterial()
+	frame1Material.SetColor(internal.NewColor(0.7098, 0.2471, 0.2196))
+	frame1Material.Diffuse = 0.6
+	frame1.SetMaterial(frame1Material)
+
+	frame2 := internal.NewCube()
+	frame2.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(-10, 3.4, 2.7),
+			internal.Scale(0.05, 0.4, 0.4),
+		),
+	)
+	frame2Material := internal.NewDefaultMaterial()
+	frame2Material.SetColor(internal.NewColor(0.2667, 0.2706, 0.6902))
+	frame2Material.Diffuse = 0.6
+	frame2.SetMaterial(frame2Material)
+
+	frame3 := internal.NewCube()
+	frame3.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(-10, 4.6, 2.7),
+			internal.Scale(0.05, 0.4, 0.4),
+		),
+	)
+	frame3Material := internal.NewDefaultMaterial()
+	frame3Material.SetColor(internal.NewColor(0.3098, 0.5961, 0.3098))
+	frame3Material.Diffuse = 0.6
+	frame3.SetMaterial(frame3Material)
+
+	frame4 := internal.NewCube()
+	frame4.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(-2, 3.5, 9.95),
+			internal.Scale(5, 1.5, 0.05),
+		),
+	)
+	frame4Material := internal.NewDefaultMaterial()
+	frame4Material.SetColor(internal.NewColor(0.3882, 0.2627, 0.1882))
+	frame4Material.Diffuse = 0.7
+	frame4.SetMaterial(frame4Material)
+
+	mirror := internal.NewCube()
+	mirror.SetTransform(
+		internal.MatrixMultiply(
+			internal.Translate(-2, 3.5, 9.95),
+			internal.Scale(4.8, 1.4, 0.06),
+		),
+	)
+	mirrorMaterial := internal.NewDefaultMaterial()
+	mirrorMaterial.SetColor(internal.NewColor(0, 0, 0))
+	mirrorMaterial.Diffuse = 0.0
+	mirrorMaterial.Ambient = 0.0
+	mirrorMaterial.Specular = 1.0
+	mirrorMaterial.Shininess = 300
+	mirrorMaterial.Reflective = 1.0
+	mirror.SetMaterial(mirrorMaterial)
+
+	objects := []internal.Shape{
+		floor,
+		wall,
+		tableTop,
+		leg1,
+		leg2,
+		leg3,
+		leg4,
+		frame1,
+		frame2,
+		frame3,
+		frame4,
+		glassCube,
+		cube1,
+		cube2,
+		cube3,
+		cube4,
+		cube5,
+		mirror,
+	}
+	world.Lights = append(world.Lights, light)
+	world.Objects = append(world.Objects, objects...)
+
+	canvas := internal.Render(camera, world)
+	writeToPng(canvas, "table.png")
+}
+
 func main() {
 	// renderCircle()
 	// renderSphere()
 	// renderScene()
 	// renderRefraction()
-	renderReflectionRefraction()
+	// renderReflectionRefraction()
+	renderTable()
 }
 
 // Adapted from https://github.com/eriklupander/rt/blob/master/main.go
